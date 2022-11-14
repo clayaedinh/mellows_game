29e3f1403c642fae85716ba2bdee34ebde85c08d
Aedin Clay <clayaedinh@gmail.com>
2022-11-14 19:47:00 +0800

    moved scanner into ignore folder, other small changes

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
 
 #include "cooking.hpp"
+#include "scanner.hpp"
+
+using namespace std;
 
 int main(){
-    printFood(cookRecipe(FOOD_FLOUR, FOOD_YEAST, FOOD_NULL));
-    printFood(cookRecipe(FOOD_FLOUR, FOOD_NULL, FOOD_NULL));
+    //printFood(cookRecipe(FOOD_FLOUR, FOOD_YEAST, FOOD_NULL));
+    //printFood(cookRecipe(FOOD_FLOUR, FOOD_NULL, FOOD_NULL));
 }
